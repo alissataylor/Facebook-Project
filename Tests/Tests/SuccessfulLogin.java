4931b7fd80bb43a66a9548ff2048f565eb4bae29
alissataylor <alissa.taylor@orasi.com>
2015-05-29 11:58:41 -0400

    Updated comments
    Updated comments to the Login test

--- Tests/Tests/SuccessfulLogin.java
+++ Tests/Tests/SuccessfulLogin.java
@@ -3,7 +3,10 @@ package Tests;
 public class SuccessfulLogin {
 
 	public static void main(String[] args) {
-		
+		/* @Author Alissa Taylor
+		 * @Summary The purpose of this is to successfully login to Facebook.
+		 * @Date 5/29/15
+		 */
 
 	}
 
