6ffde9f6bc095fc746104e4261de8b1bd91bcefa
alissataylor <alissa.taylor@orasi.com>
2015-06-09 12:00:10 -0400

    Fixing removed file

---
+++ src/test/java/tests/SuccessfulLogin.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class SuccessfulLogin {
+
+	public static void main(String[] args) {
+		/* @Author Alissa Taylor
+		 * @Summary The purpose of this is to successfully login to Facebook.
+		 * @Date 5/29/15
+		 * Testing Testing 123
+		 * Testing again 456
+		 */
+
+	}
+
+}
